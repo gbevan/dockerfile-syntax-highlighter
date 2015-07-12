5fb2950dbf3e538ddcc2f2eeef3292eb5b3371d5
Graham Bevan <graham.bevan@ntlworld.com>
2015-07-12 11:38:01 +0100

    Added support for remaining dockerfile directives.  Highlighting parsing errors. removed bordering on extra highlighted fields.  Added transparency to background coloured fields to allow selection highlighting.

--- main.js
+++ main.js
@@ -16,12 +16,119 @@ define(function (require, exports, module) {
 
     var self = {};
 
+    // Initial State Class
+    var State = function (state) {
+      if (!state) {
+        this.inDirective = false;
+        this.inEnv = false;
+        this.inLabel = false;
+        this.inAssignment = false;
+
+        this.inString = false;
+        this.startString = true;
+        this.quote = '';
+        this.upto = '';
+
+        this.localMode = null;
+        this.localState = null;
+
+        this.currentDirective = '';
+
+      } else {
+        var i;
+        for (i in state) {
+          if (state.hasOwnProperty(i)) {
+            this[i] = state[i];
+          }
+        }
+      }
+      this.previous = false;
+    };
+    State.prototype = {
+      // push current state on stack and create new state (dupl of current)
+      push: function () {
+        var newState = new State(this);
+        this.previous = newState;
+        return newState;
+      },
+      // pop previous state of the stack
+      pop: function () {
+        return this.previous;
+      }
+    };
+
     var bashMode = CodeMirror.getMode(config, {
       name: 'shell'
     });
 
+    self.endString = function (state) {
+      state.inString = false;
+      state.startString = true;
+      state.quote = '';
+    };
+
+    self.testEatString = function (stream, state) {
+      console.log('in testEatString(), inString:', state.inString, 'quote:', state.quote, 'current:', stream.current(), 'peek:', stream.peek());
+
+      var retstyle = 'string';
+
+      if (state.startString) {
+        state.startString = false;
+        if (stream.peek().match(/['"]/)) {
+          state.inString = true;
+          state.quote = stream.next();
+          console.log('start string, quote:', state.quote);
+          return 'quote';
+        }
+      }
+
+      if (state.quote !== '') {
+        var re = new RegExp('[^' + state.quote + ']');
+        if (stream.eatWhile(re)) {
+          console.log('eaten non quote');
+          if (stream.eol()) {
+            if (stream.current().slice(-1) === '\\') {
+              stream.next();
+            }
+          }
+          return retstyle;
+        }
+
+        var re_q = new RegExp('[' + state.quote + ']');
+        var m_q = stream.match(re_q, false);
+        console.log('m_q:', m_q);
+        if (stream.match(re_q, false)) {
+          console.log('eos');
+          stream.next();
+          state.inString = false;
+          state.startString = true;
+          state.quote = '';
+          return 'quote';
+        }
+      } else {
+        console.log('unquoted string:', stream.current());
+        if (stream.eatWhile(/\S+/)) {
+          console.log('eaten non quote');
+          if (stream.eol()) {
+            if (stream.current().slice(-1) === '\\') {
+              stream.next();
+            }
+          }
+          return retstyle;
+        }
+
+        console.log('eos');
+        state.inString = false;
+        state.startString = true;
+        return 'quote';
+      }
+
+
+    };
+
     self.tokenize = function (stream, state) {
-      var ch;
+      var ch,
+        inQuote;
 
       if (state.localMode) {
         var res = state.localMode.token(stream, state.localState);
@@ -33,23 +140,50 @@ define(function (require, exports, module) {
         return res;
       }
 
-      if (state.inAssignment) {
-        stream.eatSpace();
-        stream.eat(/[=]/);
-        stream.eatSpace();
-        state.inAssignment = false;
-        state.inDirective = true;
-        return null;
-      }
-
-      if (state.inEnv) {
-        stream.eatWhile(/\w/);
-        state.inEnv = false;
-        state.inAssignment = true;
-        return 'def';
+      if (state.inString) {
+        var strret2 = self.testEatString(stream, state);
+        console.log('strret2:', strret2, 'eol:', stream.eol());
+        if (strret2 === 'quote') {  // end quote
+          if (state.inDirective) {
+            state.inDirective = false;
+          }
+        }
+        if (state.quote === '' && stream.eol()) {
+          console.log('strret2 eol, inEnv:', state.inEnv);
+          if (state.inEnv) {
+            state.inEnv = false;
+            self.endString(state);
+          }
+          if (state.inLabel) {
+            state.inLabel = false;
+            self.endString(state);
+          }
+        }
+        return strret2;
       }
 
       if (state.inDirective) {
+        if (!state.currentDirective.toUpperCase().match(/(ENTRYPOINT|MAINTAINER|VOLUME)/)) {
+          if (stream.peek().match(/['"]/)) {
+            var strret = self.testEatString(stream, state);
+            console.log('strret:', strret, 'eol:', stream.eol(), 'current:', stream.current(), 'state:', state);
+            if (strret) {
+              if (strret2 === 'quote' || (stream.eol() && state.quote === '')) {  // end quote
+                if (state.inDirective) {
+                  state.inDirective = false;
+                }
+              }
+              return strret;
+            }
+          }
+        }
+
+        if (stream.match(/(#)(\s*|$)/, false)) {
+          stream.skipToEnd();
+          state.inDirective = false;
+          return 'comment';
+        }
+
         // eat lines, allowed escaped eols
         ch = stream.next();
         if (stream.eol()) {
@@ -67,58 +201,104 @@ define(function (require, exports, module) {
         default:
           return 'string';
         }
+      }
 
-      } else {
-        // Comments
+      if (state.inAssignment) {
+        console.log('inAssignment');
+        console.log('eol1:', stream.eol());
+        stream.eatSpace();
+        console.log('eol2:', stream.eol());
+        stream.eat(/[=]/);
+        console.log('eol3:', stream.eol());
+        stream.eatSpace();
+        console.log('eol4:', stream.eol());
+        state.inAssignment = false;
+        //state.inDirective = true;
+        state.inString = true;
+        console.log('state:', state);
+        return null;
+      }
+
+      if (state.inEnv) {
+        console.log('inEnv eol:', stream.eol(), 'peek: >' + stream.peek() + '<');
+        if (!stream.match(/\s+/)) {
+          stream.skipToEnd();
+          state.inEnv = false;
+          return 'error';
+        }
         if (stream.match(/(#)(\s*|$)/, false)) {
           stream.skipToEnd();
+          state.inEnv = false;
           return 'comment';
         }
+        stream.eatWhile(/\w/);
+        state.inAssignment = true;
+        return 'def';
+      }
 
-
-        if (stream.sol()) {
-          var dirMatch = stream.match(/^\s*(FROM|MAINTAINER|ENV|ADD|ENTRYPOINT|EXPOSE|RUN|CMD|COPY|ENTRYPOINT|VOLUME|USER|WORKDIR|ONBUILD)/i);
-          if (dirMatch) {
-            state.currentDirective = dirMatch[1];
-            if (stream.peek() === ' ') {
-              stream.eatSpace();
-            }
-            if (!stream.eol()) {
-              if (dirMatch[1].match(/RUN|CMD/i)) {
-                state.localMode = bashMode;
-                state.localState = bashMode.startState();
-
-              } else if (dirMatch[1].match(/ENV/i)) {
-                state.inEnv = true;
-
-              } else {
-                state.inDirective = true;
-              }
-            }
-            return 'dockdir';
-          }
+      if (state.inLabel) {
+        console.log('inLabel eol:', stream.eol(), 'peek: >' + stream.peek() + '<');
+        if (!stream.match(/\s+/)) {
+          stream.skipToEnd();
+          state.inLabel = false;
+          return 'error';
         }
+        if (stream.match(/(#)(\s*|$)/, false)) {
+          stream.skipToEnd();
+          state.inLabel = false;
+          return 'comment';
+        }
+        if (stream.peek() !== '"') {
+          stream.eatWhile(/[\w\.\-]/);
+        } else {
+          state.inString = true;
+        }
+        state.inAssignment = true;
+        return 'def';
+      }
 
+      // Comments
+      if (stream.match(/(#)(\s*|$)/, false)) {
+        stream.skipToEnd();
+        return 'comment';
       }
 
 
-      // eat the rest
+      if (stream.sol()) {
+        var dirMatch = stream.match(/^\s*(FROM|MAINTAINER|ENV|ADD|ENTRYPOINT|EXPOSE|RUN|CMD|COPY|ENTRYPOINT|VOLUME|USER|WORKDIR|ONBUILD|LABEL)/i);
+        if (dirMatch) {
+          state.currentDirective = dirMatch[1];
+          if (!dirMatch[1].match(/(ENV|LABEL)/) && stream.peek() === ' ') {
+            stream.eatSpace();
+          }
+          if (!stream.eol()) {
+            if (dirMatch[1].match(/RUN|CMD/i)) {
+              state.localMode = bashMode;
+              state.localState = bashMode.startState();
+
+            } else if (dirMatch[1].match(/ENV/i)) {
+              state.inEnv = true;
+
+            } else if (dirMatch[1].match(/LABEL/i)) {
+              state.inLabel = true;
+
+            } else {
+              state.inDirective = true;
+            }
+          }
+          return 'dockdir';
+        }
+      }
+
+      // eat the rest - treat as unparsed errors
       stream.eat(/./);
-      return null;
+      return 'error';
     };
 
     // Start it all
     return {
       startState: function () {
-        var state = {};
-
-        state.inDirective = false;
-        state.inEnv = false;
-        state.inAssignment = false;
-        state.localMode = null;
-        state.localState = null;
-
-        state.currentDirective = '';
+        var state = new State();
 
         return state;
       },
@@ -132,7 +312,7 @@ define(function (require, exports, module) {
   LanguageManager.defineLanguage("dockerfile", {
     name: "Dockerfile",
     mode: "dockerfile_plus",
-    fileNames: ["Dockerfile"],
+    fileNames: ["Dockerfile", "Dockerfile.bak", "Dockerfile.1"],
     lineComment: ["#"]
   });
 
